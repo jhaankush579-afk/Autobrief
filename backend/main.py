5b29221aff78e72b80b07edd3a1437d03c1f1403
Ankush Jha <jhaankush579@gmail.com>
2026-04-05 17:58:53 +0530

    fix cors of production

--- backend/main.py
+++ backend/main.py
@@ -53,7 +53,7 @@ app = FastAPI(title="AutoBrief API", version="1.0.0", lifespan=lifespan)
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
